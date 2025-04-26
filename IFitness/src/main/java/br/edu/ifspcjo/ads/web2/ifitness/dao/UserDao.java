234360b6914ffa8a1b57d7c7a4f553acad3dd3df
CJ3025349 <CJ3025349@192.168.56.1>
2025-04-25 21:44:58 -0300

    Alteração aula 25/04/25

--- IFitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/dao/UserDao.java
+++ IFitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/dao/UserDao.java
@@ -3,12 +3,14 @@ package br.edu.ifspcjo.ads.web2.ifitness.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
 import br.edu.ifsocjo.web2.ifitness.model.User;
+import br.edu.ifspcjo.ads.web2.ifitness.utils.PasswordEncoder;
 
 public class UserDao {
 
@@ -18,13 +20,34 @@ public class UserDao {
 		super();
 		this.dataSource = dataSource;
 	}
-		
-	public Boolean save(User user){
-	
-		String sql = "insert into user (name, email, password, "
-				+ "birth_date, gender, active) values (?,?,?,?,?,?)";
-		try(Connection conn = dataSource.getConnection(); 
-				PreparedStatement ps = conn.prepareStatement(sql)){
+
+	public Optional<User> getUserByEmail(String email) {
+		String sql = "select id,email from user where email=?";
+		Optional<User> optional = Optional.empty();
+		try (Connection con = dataSource.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, email);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					User user = new User();
+					user.setId(rs.getLong(1));
+					user.setEmail(rs.getString(2));
+					optional = Optional.of(user);
+				}
+			}
+			return optional;
+		} catch (SQLException sqlException) {
+			throw new RuntimeException("Erro durante a consulta", sqlException);
+		}
+	}
+
+	public Boolean save(User user) {
+		// verificar se existe um usuário com o mesmo e-mail
+		Optional<User> optional = getUserByEmail(user.getEmail());
+		if (optional.isPresent()) {
+			return false;
+		}
+		String sql = "insert into user (name, email, password, " + "birth_date, gender, active) values (?,?,?,?,?,?)";
+		try (Connection conn = dataSource.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
 			ps.setString(1, user.getName());
 			ps.setString(2, user.getEmail());
 			ps.setString(3, user.getPassword());
@@ -32,9 +55,34 @@ public class UserDao {
 			ps.setString(5, user.getGender().toString());
 			ps.setBoolean(6, true);
 			ps.executeUpdate();
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new RuntimeException("Erro durante a escrita no BD", e);
 		}
 		return true;
 	}
+
+	public Optional<User> getUserByEmailAndPassword(String email, String password) {
+		String passwordEncripted = PasswordEncoder.encode(password);
+
+		String sql = "select id,name,email from user where email=? and password=?";
+		Optional<User> optional = Optional.empty();
+		try (Connection con = dataSource.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, email);
+			ps.setString(2, passwordEncripted);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					User user = new User();
+					user.setId(rs.getLong(1));
+					user.setName(rs.getString(2));
+					user.setEmail(rs.getString(3));
+					optional = Optional.of(user);
+				}
+			}
+			return optional;
+		} catch (SQLException sqlException) {
+			throw new RuntimeException("Erro durante a consulta no BD", sqlException);
+		}
+	}
+
+
 }
